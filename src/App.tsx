030ac49a6fdabb100e629a33a9f9c578891adde7
dekaili <lidekai2008@gmail.com>
2025-05-06 16:43:54 -0400

    Update frontend components and configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
+import { Button } from "./components/ui/button";
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react';
 
 function App() {
-
-
   return (
     <>
-      <h1 className ="text-red-500 text-5xl" > hello</h1>
+      <header>
+        <SignedOut>
+          <SignInButton>
+            <Button>Sign In</Button>
+          </SignInButton>
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </header>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
